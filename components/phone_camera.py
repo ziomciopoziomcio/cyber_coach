f1326a164da455d94693088e723f957b496e6464
Jędrzej Niedzielski <jedrzejnie@gmail.com>
2025-12-02 19:39:22 +0100

    Class IPWebcamClient initial commit

--- components/phone_camera.py
+++ components/phone_camera.py
@@ -14,3 +14,21 @@ import time
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+class IPWebcamClient:
+
+    def __init__(self, ip_webcam_url: str = "http://192.168.1.100:8080"):
+        """
+        Inicjalizacja klienta IP Webcam.
+
+        Args:
+            ip_webcam_url: URL do IP Webcam (np. http://192.168.1.100:8080)
+        """
+        self.base_url = ip_webcam_url.rstrip('/')
+        self.video_url = f"{self.base_url}/video"
+        self.shot_url = f"{self.base_url}/shot.jpg"
+
+        self.current_frame: Optional[np.ndarray] = None
+        self.frame_callback: Optional[Callable] = None
+        self.is_running = False
+        self._stream_thread = None
+
