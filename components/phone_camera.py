3c1b0b8d58971c32d59ba01e062c50fcb6d442ae
Jędrzej Niedzielski <jedrzejnie@gmail.com>
2025-12-06 22:51:17 +0100

    Unused import deleted

--- components/phone_camera.py
+++ components/phone_camera.py
@@ -8,10 +8,6 @@ Wspiera również IP Webcam (MJPEG stream).
 
 import cv2
 import numpy as np
-import base64
-from flask import Flask, request, jsonify
-from flask_socketio import SocketIO, emit
-from flask_cors import CORS
 import threading
 from typing import Optional, Callable
 import logging
